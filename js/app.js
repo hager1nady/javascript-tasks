a49bdbfbda4d9c8202681a8990c76e5f87a67974
hager1nady <https://github.com/hager1nady>
2025-01-23 15:52:51 +0200

    add task20

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import { a, b, sayHello } from './main.js'
+console.log(a);
+console.log(b);
+console.log(sayHello());
---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+
+
+export let a = "a"
+export let b = "b"
+export let c = "c"
+export function sayHello() {
+    console.log("Hello");
+}
+
